9f06917b372aec7e8b0eefac81ce38b601fc218b
Lukáš Moláček <lucaash@gmail.com>
2019-02-10 19:44:04 +0100

    Create Flask app

--- yts/__init__.py
+++ yts/__init__.py
@@ -1,1 +1,30 @@
+#!/usr/bin/env python3
+
 import os
+from flask import Flask, render_template
+
+
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def main():
+        return(
+            render_template('index.html')
+        )
+
+    from . import api
+    app.register_blueprint(api.bp)
+
+    return(app)
